8dc73b7cc5b65b8d60eab75dfbf285bfba4ba2dc
Austin Mordahl <amordahl@uic.edu>
2025-09-17 11:40:23 -0500

    Mac os specific tests

--- src/test/scala/edu/uic/MainTests.scala
+++ src/test/scala/edu/uic/MainTests.scala
@@ -4,10 +4,13 @@ import munit.{FunSuite, ScalaCheckSuite}
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends FunSuite:
+
+  val macOsSpecific = munit.Tag("macos")
+
   test("Basic math"):
     assert(1 + 1 == 2)
 
-  test("Only pass on MacOS"):
+  test("Only pass on MacOS".tag(macOsSpecific)):
     val prop = System.getProperty("os.name")
     println(prop)
     assert(prop.toLowerCase().contains("mac"))
